da067646d8fc2a62a7da271bf327ea938c68bc3c
Rafael Vilches <rvilches38@yahoo.com.mx>
2022-11-18 02:10:57 -0600

    add delete icon and event

--- script.js
+++ script.js
@@ -19,11 +19,9 @@
         titleTask.classList.add('task');
         titleTask.innerText = value;
         taskContent.appendChild(titleTask);
-
-        const content = `                    
-                    <i class="fas fa-trash-alt trashIcon icon"></i>`;
-        // task.innerHTML = content;
+        
         task.appendChild(taskContent);
+        task.appendChild(deleteIcon());
 
         list.appendChild(task);
 
@@ -43,6 +41,18 @@
         element.classList.toggle("fas");
         element.classList.toggle("completeIcon");
         element.classList.toggle("far");
+    };
+
+    const deleteIcon = () => {
+        const i = document.createElement("i");
+        i.classList.add("fas", "fa-trash-alt", "trashIcon", "icon");
+        i.addEventListener('click', deleteTask);
+        return i;
+    };
+
+    const deleteTask = (event) => {
+        const parent = event.target.parentElement;
+        parent.remove();
     };
 
 })();
