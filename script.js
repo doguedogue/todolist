b373d3a934e9ac38be60d05eac0b80a2d7a2166c
Rafael Vilches <rvilches38@yahoo.com.mx>
2022-11-18 01:18:50 -0600

    arrow function & prevent default behav

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const btn = document.querySelector('[data-form-btn]');
 
 console.log(btn);
 
-btn.addEventListener('click', function () {
+btn.addEventListener('click', (event) => {
+    event.preventDefault();
     const input = document.querySelector('[data-form-input]');
     console.log(input.value);
 });
