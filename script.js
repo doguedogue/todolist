a3bfe4585d3dc674a0e725c8d12e8427257103d5
Rafael Vilches <rvilches38@yahoo.com.mx>
2022-11-18 01:20:55 -0600

    anonymous function

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const btn = document.querySelector('[data-form-btn]');
 
-console.log(btn);
-
-btn.addEventListener('click', (event) => {
+const createTask = (event) => {
     event.preventDefault();
     const input = document.querySelector('[data-form-input]');
-    console.log(input.value);
-});
+    const value = input.value;
+    input.value = "";
+    console.log(value);
+};
+
+btn.addEventListener('click', createTask);
