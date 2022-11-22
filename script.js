11fd34225f6472977d7e2af689db272aed3f8dae
Rafael Vilches <rvilches38@yahoo.com.mx>
2022-11-22 13:53:19 -0600

    add LocalStorage

--- script.js
+++ script.js
@@ -1,34 +1,9 @@
-import checkComplete from './components/checkComplete.js';
-import deleteIcon from './components/deleteIcon.js';
-
+import { addTask } from './components/addTask.js';
+import { displayTasks } from './components/displayTasks.js';
 
 const btn = document.querySelector('[data-form-btn]');
 
-const createTask = (event) => {
-    event.preventDefault();
-    const input = document.querySelector('[data-form-input]');
-    const value = input.value;
-    input.value = "";
+//Arrow functions o funciones anonimas
+btn.addEventListener('click', addTask);
 
-    const list = document.querySelector('[data-list]');
-
-    const task = document.createElement('li');
-    task.classList.add('card');
-
-    const taskContent = document.createElement('div');
-    taskContent.appendChild(checkComplete());
-
-    const titleTask = document.createElement('span');
-    titleTask.classList.add('task');
-    titleTask.innerText = value;
-    taskContent.appendChild(titleTask);
-    
-    task.appendChild(taskContent);
-    task.appendChild(deleteIcon());
-
-    list.appendChild(task);
-
-};
-
-btn.addEventListener('click', createTask);
-
+displayTasks();
